6705efc160d011c69a1cc27b3bf8a26b17706e1d
eyob <eyobweldiebelay@gmail.com>
2026-08-28 03:58:40 +0300

    authroute rolemidleware

--- backend/route/authRoute.js
+++ backend/route/authRoute.js
@@ -4,5 +4,6 @@ const router=express.Router()
 const authController=require('../controller/authController')
 //create route API
 router.post("/user",authController.createUser);
+router.post("/login",authController.login)
 //export router
 module.exports=router
