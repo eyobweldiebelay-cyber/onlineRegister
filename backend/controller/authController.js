46cd2d1efd22b0d755deb14345f0b86a54338f05
eyob <eyobweldiebelay@gmail.com>
2026-08-28 03:52:10 +0300

    authcontroller

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,16 +1,50 @@
-const {createService}=require('../service/authService') 
+const {userService,loginService}=require('../service/authService') 
  const createUser=async(req,res)=>{
 
-  
-    try {
-         const result=await createService();
-       return res.status(200).json(result)
+   try {
+        const{username,email,password,role}=req.body;
         
+  const result= await userService(username,email,password,role); 
+    
+     res.status(200).json({
+        message:"Inserted is Successfull Eyob",
+         success:true,
+         userid:result.userid
+        
+    });
     } catch (error) {
-       console.log(error)
-        res.status(403).json({msg:"server error",
-            message:error.message
-        })
+        console.log(error)
+          if (error.message=="Email Aleardy Exist") {
+            res.status(400).json({message:message.error});
+            
+          }
+     res.status(500).json({message:"server Error",
+                   error:error.message
+     });
+
+    }}
+    ////////////////login controller/////////////////////////
+ const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const result = await loginService(email, password);
+
+        res.status(200).json({
+            msg: "Login successful",
+            success: true,
+            data: result
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(401).json({
+            message: error.message,
+            success: false
+        });
     }
 };
-module.exports={createUser};
+module.exports={createUser,login};
