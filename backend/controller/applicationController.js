80e8a01ad2fddf5f7b6f97f36be7f48165a5ba93
eyob <eyobweldiebelay@gmail.com>
2026-08-30 04:52:54 +0300

    authmiddleware

--- backend/controller/applicationController.js
+++ backend/controller/applicationController.js
@@ -1,13 +1,87 @@
-const {applicationService}=require('../service/applicationService')
+const {createService,getMyApplication,submitApplication}=require('../service/applicationService')
 const application=async(req,res)=>{
- try {
-    const applicate=await applicationService();
-    return res.status(200).json(applicate)
- } catch (error) {
-    res.status(402).json({msg:"server error",
-        message:error.message
-    })
-    
- }
+  try {
+
+        const { program_id, academic_year } = req.body;
+
+        const studentId = req.user.student_id;
+
+        if (!program_id || !academic_year) {
+            return res.status(400).json({
+                message: "Program and academic year are required"
+            });
+        }
+
+        const result = await createService(studentId,program_id,academic_year);
+
+        res.status(201).json({
+            message: "Application created successfully",
+            application_id: result.insertId
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to create application"
+        });
+    }
 };
-module.exports={application}
+const getMyApplicationController = async (req, res) => {
+
+    try {
+
+        const studentId = req.user.student_id;
+
+        const result = await getMyApplication(studentId);
+
+        res.status(200).json({
+            applications: result
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to get application"
+        });
+    }
+};
+
+
+const submitApplicationController = async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const studentId = req.user.student_id;
+
+        const result =
+            await submitApplication(
+                id,
+                studentId
+            );
+
+        if (result.affectedRows === 0) {
+            return res.status(400).json({
+                message: "Application cannot be submitted"
+            });
+        }
+
+        res.status(200).json({
+            message: "Application submitted successfully"
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to submit application"
+        });
+    }
+};
+module.exports={application,getMyApplicationController,submitApplicationController}
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,13 +2,14 @@
 const jwt=require('jsonwebtoken')
 const authMiddleware=(req,res,next)=>{
     const authentcate=req.headers.authorization;
-    if (!authentcate) {
-        return res.status(402).json({msg:"invalid Authoriztion so Try to Login Eyob"});
+    if (!authentcate|| authentcate.startWith('Bearer')){
+        return res.status(402).json({msg:"Invalid Authoriztion so Try to Login Eyob"});
         
     }
      const token=authentcate.split(" ")[1];
+     console.log("Authorization:", req.headers.authorization);
     try {
-       const{user_id,username,email,role}=JsonWebTokenError.verfiy(token,process.env.SECURT_KEY);
+       const{user_id,username,email,role}=jwt.verfiy(token,process.env.SECURT_KEY);
         req.user={user_id,username,email,role};
     } catch (error) {
         return res.status(402).json({msg:"invalid Authoriztion from catch block so Try to Login Eyob"})
