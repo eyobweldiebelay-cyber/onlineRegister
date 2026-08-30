085ee469dd1ffb0f3392b7e6b2f238677c43d935
eyob <eyobweldiebelay@gmail.com>
2026-08-30 04:49:22 +0300

    documment route

--- backend/route/documentRoute.js
+++ backend/route/documentRoute.js
@@ -1,8 +1,14 @@
 const express=require('express')
 const router=express.Router()
+//uploamiddleware
+const upload = require("../middleware/uploadMiddleware");
 //application controller
 const documentController=require('../controller/documentController')
 //create route
-router.post("/document",documentController.document)
+router.post("/upload",documentController.document)
+//getdocument
+router.get("/getupload",documentController.getMyDocuments);
 //module export
+//upload
+
 module.exports=router
